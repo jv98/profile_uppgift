3525cf38987536ccb861086e4a84c48a86350895
jv98 <jacob@vahlen.se>
2020-11-09 16:27:21 +0100

    la till fill för img map

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let T = new Date();
 
-   document.getElementById("Timeday").innerHTML = T.getHours();
+document.getElementById("Timeday").innerHTML = T.getHours();
+myFunction();   
 function myFunction() {
     let x = new Date().getHours()
+    let y = document.getElementById("Timeday");
+
     if (x > 8  &&  x < 12 ) {
-        
+        y.innerHTML = "God Morgon";
+
     }
-    else if (x > 8  &&  x < 12 ) {
-        
+    else if (x >= 12  &&  x < 18 ) {
+        y.innerHTML = "God Eftermidag";
     }
-    else if (x > 8  &&  x < 12 ) {
-        
+    else if (x >=18   &&  x < 22 ) {
+        y.innerHTML = "God Kväll";
     }
-    else if (x > 8  &&  x < 12 ) {
-        
+    else if (x >=22  || x <= 8 ) {
+        y.innerHTML = "God Natt ";
     }
+    
   }
