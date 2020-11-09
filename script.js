a9e944b06750f4fc391562a275abdc6827886843
jv98 <jacob@vahlen.se>
2020-11-09 16:03:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let T = new Date();
+
+   document.getElementById("Timeday").innerHTML = T.getHours();
+function myFunction() {
+    let x = new Date().getHours()
+    if (x > 8  &&  x < 12 ) {
+        
+    }
+    else if (x > 8  &&  x < 12 ) {
+        
+    }
+    else if (x > 8  &&  x < 12 ) {
+        
+    }
+    else if (x > 8  &&  x < 12 ) {
+        
+    }
+  }
